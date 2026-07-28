d72f6a595b3fd8b2b2eb86d8a99f0ef1725a2b15
Abdoulbasti MUKAILA <abdoulbassit.mukaila@gmail.com>
2026-07-28 15:50:24 +0200

    test(backend): outillage des tests d'integration (supertest, mongodb-memory-server, script, env)

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/src/test"],
+  setupFiles: ["<rootDir>/src/test/setupEnv.ts"],
+  testTimeout: 30000,
   transform: {
     "^.+\\.ts$": ["ts-jest", { tsconfig: "<rootDir>/src/test/tsconfig.json" }]
   }
